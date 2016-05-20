c51ec438935fb23d1719c8098af619dfe972f57f
Joedang <shields6@pdx.edu>
2016-05-19 21:40:21 -0700

    added directory for AIAA paper, and did some prep

--- testing/LU16-10_analysis.R
+++ testing/LU16-10_analysis.R
@@ -7,6 +7,8 @@ setwd("~/PSAS/sw-cad-airframe-lv3.0/testing")
 # op <- options(digits.secs=3)
 # dat <- read.csv(commandArgs(T))
 
+pdf("LU16-10_crushAnalysis.pdf")
+
 # read and format the data
 dat <- read.csv("LV3_LU16-10_ultimateFailure.txt", skip= 5)
 # dat$Time <- paste("2016-04-29", dat$Time)
@@ -20,11 +22,26 @@ colnames(dat) <- c("Row", "Time", "BottomGauge", "MiddleGauge", "empty", "LoadKi
 ind.ult <- which.max(dat$BottomGauge)
 dat <- dat[1:ind.ult, ]
 
-plot(dat$Time, dat$BottomGauge, type="l")
+plot(
+     dat$Time, dat$BottomGauge, 
+     type="l",
+     main="strain for LU16.10",
+     xlab="time (mm:ss)",
+     ylab= 'micro-strain'
+     )
 lines(dat$Time, dat$MiddleGauge)
 points(dat$Time[ind.ult], dat$BottomGauge[ind.ult], pch=4)
 points(dat$Time[ind.ult], dat$MiddleGauge[ind.ult], pch=4)
 # lines(dat$Time, dat$LoadKips*1e3)
+
+plot(
+     dat$Time, dat$Load,
+     type="l",
+     xlab= "time (mm:ss)",
+     ylab="load (kip)",
+     main= "loading for LU16.10"
+     )
+points(dat$Time[ind.ult],dat$Load[ind.ult], pch=4)
 
 # plot(
 # 	dat$Time, dat$LoadKips*1e3, 
@@ -34,15 +51,43 @@ points(dat$Time[ind.ult], dat$MiddleGauge[ind.ult], pch=4)
 # 	)
 # lines(dat$Time, dat$BottomGauge)
 
-mm <- lm(LoadKips~MiddleGauge+0, data = dat)
-plot(dat$MiddleGauge, dat$LoadKips, type="l", bty="n", col=NA, xlim=range(dat$BottomGauge))
+mm <- lm(LoadKips~MiddleGauge, data = dat)
+plot(
+     dat$MiddleGauge, dat$LoadKips, 
+     type="l", bty="n", col=NA, 
+     xlim=range(dat$BottomGauge), 
+     main= "load-strain for LU16.10",
+     xlab= "micro-strain",
+     ylab= "load (kip)"
+     )
 abline(mm, col="red")
 points(dat$MiddleGauge[ind.ult], dat$LoadKips[ind.ult], pch=4)
-lines(dat$MiddleGauge, dat$LoadKips)
-summary(mm)
+lines(dat$MiddleGauge, dat$LoadKips, col="blue")
+# summary(mm)
 
 # plot(dat$BottomGauge, dat$LoadKips, type="l", bty="n")
-mb <- lm(LoadKips~BottomGauge+0, data = dat)
+mb <- lm(LoadKips~BottomGauge, data = dat)
 abline(mb, col="red")
 points(dat$BottomGauge[ind.ult], dat$LoadKips[ind.ult], pch=4)
-lines(dat$BottomGauge, dat$LoadKips)
+lines(dat$BottomGauge, dat$LoadKips, col="green")
+
+legend(
+       "bottomright",
+       col=c("blue","green", "red"),
+       legend=c("middle gauge", "edge gauge", "best fit"),
+       lty=c(1)
+       )
+
+cat("\nLikely load cell offset is between \n", mm$coeff["(Intercept)"], "\nand\n", mb$coeff["(Intercept)"], "\n")
+cat(
+    "\nultimate load: ", dat$Load[ind.ult]-mb$coeff["(Intercept)"],
+    "\nultimate micro-strain (edge): ", dat$BottomGauge[ind.ult],
+    "\nultimate micro-strain (middle): ", dat$MiddleGauge[ind.ult],
+    "\nload per micro-strain (edge): ", mb$coeff["BottomGauge"],
+    "\nload per micro-strain (middle): ", mm$coeff["MiddleGauge"],
+    "\n"
+    )
+
+
+
+dev.off()
